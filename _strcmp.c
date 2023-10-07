0ef74480eb0934de9b115e7c5f01c295bf32b14b
Drihmia <drihmia.redouane@gmail.com>
2023-10-07 22:46:54 +0100

    functions that compare to str

--- _strcmp.c
+++ _strcmp.c
@@ -1,4 +1,22 @@
 #include "main.h"
+/**
+ * _strcmp - compares n chars in both strings.
+ * @s1: 1st string.
+ * @s2: 2nd string.
+ * Return: difference between the total value based on ASCII of both strings.
+ * 0 if they match until n.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*(s1 + i) == *(s2 + i) && *(s2 + i) != '\0' && *(s1 + i) != '\0')
+	{
+		i++;
+	}
+	return (*(s1 + i) - *(s2 + i));
+}
 
 /**
  * _strncmp - compares n chars in both strings.
